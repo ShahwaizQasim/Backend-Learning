85a577d702885299bfd1ab3d10c83e09b1e40655
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-01 02:16:52 +0500

    work in process

---
+++ src/config/authentication.js
@@ -0,0 +1,11 @@
+
+const AuthenticationUser = async (req, res, next) => {
+    try {
+        console.log("req.headers=>", req.headers);
+
+    } catch (error) {
+
+    }
+}
+
+export default AuthenticationUser
--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -4,12 +4,15 @@ import AddUsers from "./controllers/post.js";
 import LoginUsers from "./controllers/login.js";
 import deleteUser from "./controllers/deleteUser.js";
 import updateUser from "./controllers/updateUser.js";
+import FindUser from "./controllers/findUser.js";
+import AuthenticationUser from "../../config/authentication.js";
 
 const userRoutes = Router()
 
 userRoutes.post('/login', LoginUsers)
 userRoutes.post('/', AddUsers)
-userRoutes.get('/', getUsers)
+userRoutes.get('/', AuthenticationUser, getUsers)
+userRoutes.get('/UserInfo', AuthenticationUser, FindUser)
 userRoutes.delete('/:id', deleteUser)
 userRoutes.put('/:id', updateUser)
 
