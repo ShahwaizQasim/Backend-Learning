af4094c7f53b53aa0d34e51cad5820603ef75e64
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-18 12:20:51 +0500

    delete api Course

--- src/modules/addCourse/index.js
+++ src/modules/addCourse/index.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import AddCourse from "./controllers/post.js";
 import getCourse from "./controllers/get.js";
 import { AuthenticationAdmin, AuthenticationUsers } from "../../config/authentication.js";
+import DeleteUser from "./controllers/delete.js";
 
 const courseRoutes = Router();
 
 courseRoutes.post('/', AuthenticationAdmin, AddCourse);
 courseRoutes.get('/', AuthenticationUsers, getCourse);
+courseRoutes.delete('/:id', AuthenticationAdmin, DeleteUser);
 
 export default courseRoutes;
