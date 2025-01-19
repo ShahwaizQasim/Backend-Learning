3ff5f5a086540ccd4cb8bb0eb12db5b3aa5436e1
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-19 11:29:08 +0500

    logics add

--- src/modules/addCourse/controllers/updateCourse.js
+++ src/modules/addCourse/controllers/updateCourse.js
@@ -6,6 +6,9 @@ const UpdateCourse = async (req, res) => {
     try {
         const { id } = req.params;
         const UpdateCourse = await CourseModel.findByIdAndUpdate(id, req.body)
+        if (!UpdateCourse) {
+            res.status(404).send({ status: 404, message: "course not found" });
+        }
         res.status(200).send({ status: 200, message: "course updated successfully" });
     } catch (error) {
         res.status(400).send({ status: 400, message: error });
